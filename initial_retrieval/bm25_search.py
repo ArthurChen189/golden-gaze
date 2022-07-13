5904c7a9c4c079cbc13e981468d8a48074f0c8c5
Arthur Chen <479711712@qq.com>
2022-07-13 21:07:48 +0000

    enable batch init retrieval

--- initial_retrieval/bm25_search.py
+++ initial_retrieval/bm25_search.py
@@ -10,11 +10,11 @@ from pyserini.search import SimpleSearcher
 # Set global variables
 parser = argparse.ArgumentParser()
 parser.add_argument('-T', '--topic_set', help='Topic Set', choices=['2019', '2021', 'WH'], required=True)
-parser.add_argument('-t', '--topic_id', help='Topic ID', required=True)
+# parser.add_argument('-t', '--topic_id', help='Topic ID', required=True)
 parser.add_argument('-i', '--index_path', help='Path to the index', required=True)
 args = parser.parse_args()
 k = 3000  # Retrieve top k documents per topic
-
+topic_ids = [];
 
 # Load topics
 if args.topic_set == '2019':
@@ -28,6 +28,7 @@ if args.topic_set == '2019':
     topics_stance['correct_stance'] = topics_stance['correct_stance'].map({1: 'helpful', 0: 'inconclusive',
                                                                            -1: 'unhelpful'})
     topics = pd.merge(topics, topics_stance, how='left', on='topic_id')
+    topic_ids = [i for i in range(1,52)]
 elif args.topic_set == '2021':
     xml_root = et.parse('../data/raw_data/misinfo-2021-topics.xml')
     rows = xml_root.findall('topic')
@@ -35,37 +36,39 @@ elif args.topic_set == '2021':
                  row.find('narrative').text, row.find('stance').text, row.find('evidence').text] for row in rows]
     topics = pd.DataFrame(xml_data,
                           columns=['topic_id', 'query', 'description', 'narrative', 'correct_stance', 'evidence'])
+    topic_ids = [i for i in range(101,151)]
+
 elif args.topic_set == 'WH':
     k = 100  # Only the top 100 documents are needed
     topics = pd.read_csv('../data/WH_topics.csv')
     topics['description'] = 'none'
+    topic_ids = [i for i in range(0,89)]
 else:
     raise ValueError('[ERROR] Invalid parameter "topic_set".')
 
 
-# Specify the topic
-topics = topics[topics['topic_id'] == int(args.topic_id)]
-topic_id = int(args.topic_id)
-query = topics['query'].values.item()
-description = topics['description'].values.item()
-correct_stance = topics['correct_stance'].values.item()
-
 # Start searching
 searcher = SimpleSearcher(args.index_path)
 searcher.set_bm25(0.9, 0.4)
-hits = searcher.search(query, k=k)
 
-records = []
-for i in tqdm(range(k)):
-    doc_id = hits[i].docid
-    doc_json = json.loads(searcher.doc(doc_id).raw())
-    records.append([topic_id, doc_id, i + 1, hits[i].score, query, description, correct_stance, doc_json['text'],
-                    doc_json['timestamp'], doc_json['url']])
+for topic_id in topic_ids:
 
-output_dir = Path(f'./output/{args.topic_set}')
-output_dir.mkdir(parents=True, exist_ok=True)
-dataset = pd.DataFrame(records,
+    temp_topics = topics[topics['topic_id'] == topic_id]
+    query = temp_topics['query'].values.item()
+    description = temp_topics['description'].values.item()
+    correct_stance = temp_topics['correct_stance'].values.item()
+
+    hits = searcher.search(query, k=k)
+    records = []
+    for i in tqdm(range(k)):
+        doc_id = hits[i].docid
+        doc_json = json.loads(searcher.doc(doc_id).raw())
+        records.append([topic_id, doc_id, i + 1, hits[i].score, query, description, correct_stance, doc_json['text'],
+                        doc_json['timestamp'], doc_json['url']])
+
+    output_dir = Path(f'./output/{args.topic_set}')
+    output_dir.mkdir(parents=True, exist_ok=True)
+    dataset = pd.DataFrame(records,
                        columns=['topic_id', 'doc_id', 'rank', 'score', 'query', 'description', 'correct_stance', 'text',
                                 'timestamp', 'url'])
-
-dataset.to_csv(f'./output/{args.topic_set}/{args.topic_id}.csv', index=False)
+    dataset.to_csv(f'./output/{args.topic_set}/{topic_id}.csv', index=False)
